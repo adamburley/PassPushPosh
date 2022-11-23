4a1aecda97a995c90a542e6a08b1d4b2a0acf8f1
adamburley <52085927+adamburley@users.noreply.github.com>
2022-11-23 14:07:17 -0500

    Documentation build tweaks

--- Docs/README.md
+++ Docs/README.md
@@ -1,57 +1,38 @@
----
-Module Name: PassPushPosh
-Module Guid: 5d8a1afd-a912-440f-a9b9-e79f42a05f21
-Download Help Link: {{ Update Download Link }}
-Help Version: {{ Please enter version of help manually (X.X.X.X) format }}
-Locale: en-US
----
-
 # PassPushPosh Module
 
 ## Description
 
-{{ Fill in the Description }}
+        *PassPushPosh* is a PowerShell Module for interfacing with the Password Pusher website/application API.
+        It supports anonymous and authenticated pushes, provides verbose responses to errors, -Whatif and -Confirm,
+        and in general tries to be as "Powershell-y" as possible.
 
-## PassPushPosh Classes
+        Using *PassPushPosh* can be as simple as:
+
+        ```powershell
+        PS> Import-Module PassPushPosh
+        PS> $myPush = New-Push "Here's my secret!"
+        PS> $myPush.Link
+        https://pwpush.com/en/p/gzv65wiiuciy
+        ```
+        
+        See documentation here or `Get-Help [command]` on any function for more information. Happy sharing!
+
+## Classes
 
 ### [[PasswordPush](PasswordPush-Class.md)]
 
 Represents a Push with all metadata including Payload (password) value
 
-## PassPushPosh Cmdlets
+## Functions
 
-### [ConvertTo-PasswordPush](ConvertTo-PasswordPush.md)
-
-{{ Fill in the Description }}
-
-### [Get-Dashboard](Get-Dashboard.md)
-
-{{ Fill in the Description }}
-
-### [Get-Push](Get-Push.md)
-
-{{ Fill in the Description }}
-
-### [Get-PushAuditLog](Get-PushAuditLog.md)
-
-{{ Fill in the Description }}
-
-### [Get-SecretLink](Get-SecretLink.md)
-
-{{ Fill in the Description }}
-
-### [Initialize-PassPushPosh](Initialize-PassPushPosh.md)
-
-{{ Fill in the Description }}
-
-### [New-PasswordPush](New-PasswordPush.md)
-
-{{ Fill in the Description }}
-
-### [New-Push](New-Push.md)
-
-{{ Fill in the Description }}
-
-### [Remove-Push](Remove-Push.md)
-
-{{ Fill in the Description }}
+| Function | Summary |
+|--|--|
+| **[ConvertTo-PasswordPush](ConvertTo-PasswordPush.md)** | Convert API call response to a PasswordPush object |
+| **[Get-Dashboard](Get-Dashboard.md)** | Get a list of active or expired Pushes for an authenticated user |
+| **[Get-Push](Get-Push.md)** | Retrieve the secret contents of a Push |
+| **[Get-PushAuditLog](Get-PushAuditLog.md)** | Get the view log of an authenticated Push |
+| **[Get-SecretLink](Get-SecretLink.md)** | Returns a fully qualified secret link to a push of given URL Token |
+| **[Initialize-PassPushPosh](Initialize-PassPushPosh.md)** | Initialize the PassPushPosh module |
+| **[New-PasswordPush](New-PasswordPush.md)** | Create a new blank Password Push object. |
+| **[New-Push](New-Push.md)** | Create a new Password Push |
+| **[Remove-Push](Remove-Push.md)** | Remove a Push |
--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
-param ( [switch]$New, [switch]$NoTweaks )
+# Builds the module file and documentation.
+# Documentation still needs manual massaging after build to look right online so keep that in mind
+# Biggest issue is it not detecting the end of codeblocks properly in examplesd
+
+param ( [switch]$BuildDocs, [switch]$NoTweaks )
 Write-Host 'Building Module...' -ForegroundColor Cyan
 $PsmPath = './PassPushPosh/PassPushPosh.psm1'
 if (Test-Path $PsmPath) { Remove-item $PsmPath }
@@ -19,104 +23,152 @@ foreach($folder in @('PassPushPosh/Classes','PassPushPosh/Private', 'PassPushPos
     }
 }
 
-Import-Module ./PassPushPosh -Force
+if ($BuildDocs){
+    Import-Module ./PassPushPosh -Force
 
-
-$docPath = 'Docs'
-$parameters = @{
-
-    AlphabeticParamsOrder = $false
-    Encoding = [System.Text.Encoding]::UTF8
-    ModulePagePath = 'Docs/README.md'
-}
-if ($New) {
-    $parameters.Module = 'PassPushPosh'
-    $parameters.OutputFolder = $docPath
-    $parameters.WithModulePage = $true
-    $parameters.NoMetadata = $true
+    # Using the Update- functions in PlatyPS seems broken. Maybe it's just
+    # the modifications to the files I'm making...
+    $docPath = 'Docs'
+    Remove-Item $docPath/*
+    $parameters = @{
+        AlphabeticParamsOrder = $false
+        Encoding = [System.Text.Encoding]::UTF8
+        ModulePagePath = 'Docs/README.md'
+        Module         = 'PassPushPosh'
+        OutputFolder   = $docPath
+        WithModulePage = $false
+        NoMetadata     = $true
+    }
     Write-Host "Generating new markdown help" -ForegroundColor Cyan
     New-MarkdownHelp @parameters -force
-    #New-MarkdownAboutHelp -OutputFolder './Docs' -AboutName "PassPushPosh"
-} else {
-    $parameters.Path = $docPath
-    $parameters.RefreshModulePage = $true
-    $parameters.LogPath = '.LastHelpModuleUpdate.txt'
-    Update-MarkdownHelpModule @parameters -Force
-}
+    New-MarkdownAboutHelp -OutputFolder './Docs' -AboutName "PassPushPosh"
 
-if (-not $NoTweaks) {
-    $tweaksModuleName = 'PassPushPosh'
-    $tweaksModuleClasses = @('PasswordPush')
-    $moduleOnlineDocumentationRoot = 'https://github.com/adamburley/PassPushPosh/blob/main/Docs'
-    $moduleFunctions = Get-Command -Module $tweaksModuleName | select -ExpandProperty Name
-    
-    # Fixing some problems with the markdown PlatyPs outputs...
-    Write-Host 'Markdown tweaks...' -ForegroundColor Yellow
+    if (-not $NoTweaks) {
+        $tweaksModuleName = 'PassPushPosh'
+        $tweaksModuleClasses = @('PasswordPush')
+        $moduleOnlineDocumentationRoot = 'https://github.com/adamburley/PassPushPosh/blob/main/Docs'
+        $moduleFunctions = Get-Command -Module $tweaksModuleName | select -ExpandProperty Name
+        
+        # Fixing some problems with the markdown PlatyPs outputs...
+        Write-Host 'Markdown tweaks...' -ForegroundColor Yellow
 
-    foreach ($docFile in (Get-ChildItem -Path ./Docs -Recurse)) {
-        Write-Host '--- ' $docFile.Name ' ---'
-        $fileContent = Get-Content -Path $docFile.FullName
-        $outRows = @()
-        for ($i = 0; $i -lt $fileContent.Count; $i++) {
-            $row = $fileContent[$i]
-            if ($row -ilike "#*" -and $fileContent[$i+1] -ne '') { # Blank lines after headings
-                $outRows += $row
-                $outRows += ''
+        foreach ($docFile in (Get-ChildItem -Path ./Docs -Recurse)) {
+            Write-Host '--- ' $docFile.Name ' ---'
+            $fileContent = Get-Content -Path $docFile.FullName
+            $outRows = @()
+            for ($i = 0; $i -lt $fileContent.Count; $i++) {
+                $row = $fileContent[$i]
+                if ($row -ilike "#*" -and $fileContent[$i+1] -ne '') { # Blank lines after headings
+                    $outRows += $row
+                    $outRows += ''
+                }
+                elseif ($row -eq '```' -and $fileContent[$i+1] -ne '') {
+                    $outRows += '```powershell' # Language marker for code blocks
+                }
+                else { $outRows += $row }
             }
-            elseif ($row -eq '```' -and $fileContent[$i+1] -ne '') {
-                $outRows += '```powershell' # Language marker for code blocks
-            }
-            else { $outRows += $row }
-        }
 
-        # Fix Links
-        $thisFunctionHelp = Get-Help $docFile.BaseName
-        if ($thisFunctionHelp.relatedLinks) { # Has help links
-            $outRows = $outRows[0..$outRows.IndexOf('## RELATED LINKS')]
-            $outRows += ''
-            for ($ri = 0; $ri -lt $thisFunctionHelp.relatedLinks.navigationLink.Count; $ri++) {
-                $link = $thisFunctionHelp.relatedLinks.navigationLink[$ri]
-                if ($link.uri) { # Link was detected as a web URI
-                    if ($link.uri -ilike "$moduleOnlineDocumentationRoot*") {
-                        Write-Host "Skipping" $link.uri -ForegroundColor Yellow
-                    } else {
-                        Write-Host $link.uri -ForegroundColor DarkCyan
-                        $outRows += "- [Password Pusher API Documentation]($($link.uri))"
+            # Fix Links
+            if ($docFile.Basename -ine 'about_PassPushPosh') {
+                $thisFunctionHelp = Get-Help $docFile.BaseName
+                if ($thisFunctionHelp.relatedLinks) { # Has help links
+                    $outRows = $outRows[0..$outRows.IndexOf('## RELATED LINKS')]
+                    $outRows += ''
+                    for ($ri = 0; $ri -lt $thisFunctionHelp.relatedLinks.navigationLink.Count; $ri++) {
+                        $link = $thisFunctionHelp.relatedLinks.navigationLink[$ri]
+                        if ($link.uri) { # Link was detected as a web URI
+                            if ($link.uri -ilike "$moduleOnlineDocumentationRoot*") {
+                                Write-Host "Skipping" $link.uri -ForegroundColor Yellow
+                            } else {
+                                Write-Host $link.uri -ForegroundColor DarkCyan
+                                $outRows += "- [Password Pusher API Documentation]($($link.uri))"
+                            }
+                        } elseif ($link.linkText) {
+                            $linkString = $link.linkText.Trim()
+                            if ($linkString -iin $moduleFunctions) {
+                                Write-Host "Function: $linkString" -ForegroundColor Magenta
+                                $outRows += "- [$linkString]($linkString.md)"
+                            }
+                        } else {
+                            Write-Host "Unknown: " $link -ForegroundColor Red
+                        }
                     }
-                } elseif ($link.linkText) {
-                    $linkString = $link.linkText.Trim()
-                    if ($linkString -iin $moduleFunctions) {
-                        Write-Host "Function: $linkString" -ForegroundColor Magenta
-                        $outRows += "- [$linkString]($linkString.md)"
-                    }
-                } else {
-                    Write-Host "Unknown: " $link -ForegroundColor Red
                 }
             }
+            $outRows = ($outRows -join "`n").Trim() -split "`n" # ugly way to remove trailing whitespaces
+            $outRows | Out-File -FilePath $docFile.FullName
         }
-        $outRows = ($outRows -join "`n").Trim() -split "`n" # ugly way to remove trailing whitespaces
-        $outRows | Out-File -FilePath $docFile.FullName
-    }
 
-    # Add class references
-    $newReadmeBlock = @("## $tweaksModulename Classes","")
-    foreach ($class in $tweaksModuleClasses) {
-        Write-Host "Tweaking $class documentation" -ForegroundColor Gray
-        # Copy readme to docs
-        Copy-Item "./PassPushPosh/Classes/$class.md" -Destination "./Docs/$class-Class.md"
+        $classesReadmeBlock = @()
+        # Handle classes
+        foreach ($class in $tweaksModuleClasses) {
+            Write-Host "Building class documentation: $class " -ForegroundColor Gray
+            # Copy readme to docs
+            Copy-Item "./PassPushPosh/Classes/$class.md" -Destination "./Docs/$class-Class.md"
+            # Get summary
+            $classReadme = Get-Content "./PassPushPosh/Classes/$class.md"
+            $cRmStartIndex = $classReadme.IndexOf('## SUMMARY') + 1
+            $cRmStopIndex = $classReadme.IndexOf('## DESCRIPTION') - 1
+            $cSummary = $classReadme[$cRmStartIndex..$cRmStopIndex]
+            # Add reference to readme
+            $classesReadmeBlock += ''
+            $classesReadmeBlock += "### [[$class]($class-Class.md)]"
+            $classesReadmeBlock += $cSummary
+        }
 
-        # Get summary
-        $classReadme = Get-Content "./PassPushPosh/Classes/$class.md"
-        $cRmStartIndex = $classReadme.IndexOf('## SUMMARY') + 1
-        $cRmStopIndex = $classReadme.IndexOf('## DESCRIPTION') - 1
-        $cSummary = $classReadme[$cRmStartIndex..$cRmStopIndex]
-        Write-host $cSummary -join "`n"
-        # Add reference to readme
-        $newReadmeBlock += "### [[$class]($class-Class.md)]"
-        $newReadmeBlock += $cSummary
+        # Manually build README.md
+        $readMe = @("# $tweaksModuleName Module", '', '## Description', '')
+        $readMe += @"
+        *PassPushPosh* is a PowerShell Module for interfacing with the Password Pusher website/application API.
+        It supports anonymous and authenticated pushes, provides verbose responses to errors, `-Whatif` and `-Confirm`,
+        and in general tries to be as "Powershell-y" as possible.
+
+        Using *PassPushPosh* can be as simple as:
+
+        ``````powershell
+        PS> Import-Module PassPushPosh
+        PS> `$myPush = New-Push "Here's my secret!"
+        PS> `$myPush.Link
+        https://pwpush.com/en/p/gzv65wiiuciy
+        ``````
+        
+        See documentation here or ``Get-Help [command]`` on any function for more information. Happy sharing!
+
+"@
+
+        $readMe += '## Classes'
+        $readMe += $classesReadmeBlock
+        $readMe += '## Functions','','| Function | Summary |','|--|--|'
+        foreach ($f in $moduleFunctions) {
+            $s = Get-Help $f | select -ExpandProperty Synopsis
+            $readMe += "| **[$f]($f.md)** | $s |"
+        }
+
+        $readMe | Out-File -FilePath 'Docs/README.md'
+
+
+        # The old method
+        <#
+        # Add class references
+        $newReadmeBlock = @("## $tweaksModulename Classes","")
+        foreach ($class in $tweaksModuleClasses) {
+            Write-Host "Building class documentation: $class " -ForegroundColor Gray
+            # Copy readme to docs
+            Copy-Item "./PassPushPosh/Classes/$class.md" -Destination "./Docs/$class-Class.md"
+
+            # Get summary
+            $classReadme = Get-Content "./PassPushPosh/Classes/$class.md"
+            $cRmStartIndex = $classReadme.IndexOf('## SUMMARY') + 1
+            $cRmStopIndex = $classReadme.IndexOf('## DESCRIPTION') - 1
+            $cSummary = $classReadme[$cRmStartIndex..$cRmStopIndex]
+            # Add reference to readme
+            $newReadmeBlock += "### [[$class]($class-Class.md)]"
+            $newReadmeBlock += $cSummary
+        }
+        $readmeContent = Get-Content 'Docs/README.md'
+        $cmdLetHeaderIndex = $readmeContent.IndexOf(($readmeContent | where { $_ -ilike "## * Cmdlets"}))
+        $assembledReadme = $readmeContent[0..($cmdLetHeaderIndex-1)] + $newReadmeBlock + $readmeContent[$cmdLetHeaderIndex..($readmeContent.Count-1)]
+        $assembledReadme | Out-File -FilePath 'Docs/README.MD'
+        #>
     }
-    $readmeContent = Get-Content $parameters.ModulePagePath
-    $cmdLetHeaderIndex = $readmeContent.IndexOf(($readmeContent | where { $_ -ilike "## * Cmdlets"}))
-    $assembledReadme = $readmeContent[0..($cmdLetHeaderIndex-1)] + $newReadmeBlock + $readmeContent[$cmdLetHeaderIndex..($readmeContent.Count-1)]
-    $assembledReadme | Out-File -FilePath $parameters.ModulePagePath
 }
