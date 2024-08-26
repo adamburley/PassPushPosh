281252714f1f9b307f11984f8f4d7a53601149a3
adamburley <52085927+adamburley@users.noreply.github.com>
2024-08-26 15:40:45 -0400

    Refactoring for code consistency
    Removing try/catch blocks, -Raw return switch, etc

--- PassPushPosh/Public/Get-Push.ps1
+++ PassPushPosh/Public/Get-Push.ps1
@@ -4,15 +4,18 @@
     Retrieve the secret contents of a Push
 
     .DESCRIPTION
-    Accepts a URL Token string, returns the contents of a Push along with
-    metadata regarding that Push. Note, Get-Push will return data on an expired
+    Get-Push accepts a URL Token string and returns the contents of a Push along with
+    metadata regarding that Push. Note: Get-Push will return data on an expired
     Push (datestamps, etc) even if it does not return the Push contents.
+
+    .PARAMETER URLToken
+    URL Token for the secret
 
     .INPUTS
     [string]
 
     .OUTPUTS
-    [PasswordPush] or [string]
+    [PasswordPush]
 
     .EXAMPLE
     Get-Push -URLToken gzv65wiiuciy
@@ -34,28 +37,13 @@
     [CmdletBinding()]
     [OutputType([PasswordPush])]
     param(
-        # URL Token for the secret
-        [parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [parameter(Mandatory, ValueFromPipeline, Position = 0)]
         [ValidateNotNullOrEmpty()]
         [Alias('Token')]
         $URLToken
     )
     begin { Initialize-PassPushPosh -Verbose:$VerbosePreference -Debug:$DebugPreference }
-
     process {
-        try {
-            $response = Invoke-PasswordPusherAPI -Endpoint "p/$URLToken.json"
-            if ($DebugPreference -eq [System.Management.Automation.ActionPreference]::Continue) {
-                Set-Variable -Scope Global -Name PPPLastCall -Value $response
-                Write-Debug 'Response to Invoke-WebRequest set to PPPLastCall Global variable'
-            }
-            $response.Content | ConvertTo-PasswordPush
-        } catch {
-            Write-Verbose "An exception was caught: $($_.Exception.Message)"
-            if ($DebugPreference -eq [System.Management.Automation.ActionPreference]::Continue) {
-                Set-Variable -Scope Global -Name PPPLastError -Value $_
-                Write-Debug -Message 'Response object set to global variable $PPPLastError'
-            }
-        }
+        Invoke-PasswordPusherAPI -Endpoint "p/$URLToken.json" | Select-Object -ExpandProperty Content | ConvertTo-PasswordPush
     }
 }
