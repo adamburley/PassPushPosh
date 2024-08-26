ad802b8ecb145152a1c8e91ceeb3781348e27615
adamburley <52085927+adamburley@users.noreply.github.com>
2024-08-26 16:17:13 -0400

    Refactor
    Work on #6 and #3

--- PassPushPosh/Public/Get-SecretLink.ps1
+++ PassPushPosh/Public/Get-SecretLink.ps1
@@ -1,5 +1,4 @@
-﻿function Get-SecretLink {
-    <#
+﻿<#
     .SYNOPSIS
     Returns a fully qualified secret link to a push of given URL Token
 
@@ -8,6 +7,9 @@
     Returned value is a 1-step retrieval link depending on option selected during Push creation.
     Returns false if URL Token is invalid, however it will return a URL if the token is valid
     but the Push is expired or deleted.
+
+    .PARAMETER URLToken
+    URL Token for the secret
 
     .INPUTS
     [string] URL Token value
@@ -40,28 +42,19 @@
     As it returns false if a Push URL token is not valid you can use it to test if a Push exists without
     burning a view.
     #>
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars','',Scope='Function',Justification='Global variables are used for module session helpers.')]
+function Get-SecretLink {
     [CmdletBinding()]
     [Alias('Get-PushPreview')]
     [OutputType('[string]')]
     param(
-        # URL Token for the secret
         [parameter(Mandatory, ValueFromPipeline)]
+        [Alias('Token')]
         [ValidateLength(5, 256)]
         [string]$URLToken
     )
     begin { Initialize-PassPushPosh -Verbose:$VerbosePreference -Debug:$DebugPreference }
     process {
-        try {
-            $responseContent = Invoke-PasswordPusherAPI -Endpoint "p/$URLToken/preview.json" | Select-Object -ExpandProperty Content
-            $responseContent | ConvertFrom-Json | Select-Object -ExpandProperty url
-        }
-        catch {
-            Write-Verbose "An exception was caught: $($_.Exception.Message)"
-            if ($DebugPreference -eq [System.Management.Automation.ActionPreference]::Continue) {
-                Set-Variable -Scope Global -Name 'PPPLastError' -Value $_
-                Write-Debug -Message 'Response object set to global variable $PPPLastError'
-            }
-        }
+        $responseContent = Invoke-PasswordPusherAPI -Endpoint "p/$URLToken/preview.json" | Select-Object -ExpandProperty Content
+        $responseContent | ConvertFrom-Json | Select-Object -ExpandProperty url
     }
 }
--- PassPushPosh/Public/Remove-Push.ps1
+++ PassPushPosh/Public/Remove-Push.ps1
@@ -1,5 +1,4 @@
-﻿function Remove-Push {
-    <#
+﻿<#
     .SYNOPSIS
     Remove a Push
 
@@ -14,6 +13,12 @@
 
     If the Push URL Token is invalid OR you are not authorized to delete the
     Push, the endpoint returns 404 and this function returns $false
+
+    .PARAMETER URLToken
+    URL Token for the secret
+
+    .PARAMETER PushObject
+    PasswordPush object
 
     .INPUTS
     [string] URL Token
@@ -37,70 +42,40 @@
     .NOTES
     TODO testing and debugging
     #>
+function Remove-Push {
     [CmdletBinding(SupportsShouldProcess, DefaultParameterSetName = 'Token')]
     [OutputType([PasswordPush], [bool])]
     param(
-        # URL Token for the secret
         [parameter(ValueFromPipeline, ParameterSetName = 'Token')]
         [ValidateNotNullOrEmpty()]
         [Alias('Token')]
         [string]
         $URLToken,
 
-        # PasswordPush object
         [Parameter(ValueFromPipeline, ParameterSetName = 'Object')]
         [PasswordPush]
         $PushObject
     )
     process {
-        try {
-            if ($PSCmdlet.ParameterSetName -eq 'Object') {
-                Write-Debug -Message "Remove-Push was passed a PasswordPush object with URLToken: [$($PushObject.URLToken)]"
-                if (-not $PushObject.IsDeletableByViewer -and -not $Script:PPPHeaders) {
-                    #Pre-qualify if this will succeed
-                    Write-Warning -Message 'Unable to remove Push. Push is not marked as deletable by viewer and you are not authenticated.'
-                    return $false
-                }
-                if ($PushObject.IsDeletableByViewer) {
-                    Write-Verbose "Push is flagged as deletable by viewer, should be deletable."
-                }
-                else { Write-Verbose "In an authenticated API session. Push will be deletable if it was created by authenticated user." }
-                $URLToken = $PushObject.URLToken
+        if ($PSCmdlet.ParameterSetName -eq 'Object') {
+            Write-Debug -Message "Remove-Push was passed a PasswordPush object with URLToken: [$($PushObject.URLToken)]"
+            if (-not $PushObject.IsDeletableByViewer -and -not $Script:PPPHeaders) {
+                #Pre-qualify if this will succeed
+                Write-Warning -Message 'Unable to remove Push. Push is not marked as deletable by viewer and you are not authenticated.'
+                continue
             }
-            else {
-                Write-Debug -Message "Remove-Push was passed a URLToken: [$URLToken]"
+            if ($PushObject.IsDeletableByViewer) {
+                Write-Verbose "Push is flagged as deletable by viewer, should be deletable."
             }
-            Write-Verbose -Message "Push with URL Token [$URLToken] will be deleted if 'Deletable by viewer' was enabled or you are the creator of the push and are authenticated."
-            $iwrSplat = @{
-                'Method'      = 'Delete'
-                'ContentType' = 'application/json'
-                'Uri'         = "$Script:PPPBaseUrl/p/$URLToken.json"
-                'UserAgent'   = $Script:PPPUserAgent
-            }
-            if ($Script:PPPHeaders) { $iwrSplat['Headers'] = $Script:PPPHeaders }
-            Write-Verbose "Sending HTTP request: $($iwrSplat | Out-String)"
-            if ($PSCmdlet.ShouldProcess('Delete', "Push with token [$URLToken]")) {
-                $response = Invoke-PasswordPusherAPI -Endpoint "p/$URLToken.json" -Method 'Delete'
-                if ($DebugPreference -eq [System.Management.Automation.ActionPreference]::Continue) {
-                    Set-Variable -Scope Global -Name PPPLastCall -Value $response
-                    Write-Debug 'Response to Invoke-WebRequest set to PPPLastCall Global variable'
-                }
-                $response.Content | ConvertTo-PasswordPush
-            }
+            else { Write-Verbose "In an authenticated API session. Push will be deletable if it was created by authenticated user." }
+            $URLToken = $PushObject.URLToken
         }
-        catch {
-            if ($_.Exception.Response.StatusCode -eq 404) {
-                Write-Warning "Failed to delete Push. This can indicate an invalid URL Token, that the password was not marked deletable, or that you are not the owner."
-                return $false
-            }
-            else {
-                Write-Verbose "An exception was caught: $($_.Exception.Message)"
-                if ($DebugPreference -eq [System.Management.Automation.ActionPreference]::Continue) {
-                    Set-Variable -Scope Global -Name PPPLastError -Value $_
-                    Write-Debug -Message 'Response object set to global variable $PPPLastError'
-                }
-                $_
-            }
+        else {
+            Write-Debug -Message "Remove-Push was passed a URLToken: [$URLToken]"
+        }
+        Write-Verbose -Message "Push with URL Token [$URLToken] will be deleted if 'Deletable by viewer' was enabled or you are the creator of the push and are authenticated."
+        if ($PSCmdlet.ShouldProcess('Delete', "Push with token [$URLToken]")) {
+            Invoke-PasswordPusherAPI -Endpoint "p/$URLToken.json" -Method 'Delete' | ConvertTo-PasswordPush
         }
     }
 }
