41de816d9883db8bfa2c421037722d94677257fd
adamburley <52085927+adamburley@users.noreply.github.com>
2025-02-13 10:59:45 -0500

    Remove redundant code

--- PassPushPosh/Classes/PasswordPush.ps1
+++ PassPushPosh/Classes/PasswordPush.ps1
@@ -25,40 +25,7 @@
 
     # Constructor to allow casting or explicit import from a PSObject Representing the result of an API call
     PasswordPush([PSCustomObject]$APIresponseObject) {
-        Write-Debug 'New PasswordPush object instantiated from JsonResponse string'
-        Initialize-PassPushPosh # Initialize the module if not yet done.
-
         $_j = $APIresponseObject
-        $this.Note = $_j.note
-        $this.Payload = $_j.payload
-        $this.IsExpired = $_j.expired
-        $this.IsDeleted = $_j.deleted
-        $this.IsDeletableByViewer = $_j.deletable_by_viewer
-        $this.ExpireAfterDays = $_j.expire_after_days
-        $this.DaysRemaining = $_j.days_remaining
-        $this.ExpireAfterViews = $_j.expire_after_views
-        $this.ViewsRemaining = $_j.views_remaining
-        $this.DateCreated = $_j.created_at
-        $this.DateUpdated = $_j.updated_at
-        $this.DateExpired = if ($_j.expired_on) { $_j.expired_on } else { [DateTime]0 }
-        $this.RetrievalStep = $_j.retrieval_step
-        $this.AccountId = $_j.account_id
-        $this.UrlToken = $_j.url_token
-        $this.LinkDirect = $_j.json_url ? $_j.json_url.Replace('.json','') : "$Script:PPPBaseUrl/p/$($this.__UrlToken)"
-        $this.LinkRetrievalStep = $this.LinkDirect, '/r' -join ''
-        $this.Link = $_.html_url
-
-        if ($_j.Files) {
-            $this | Add-Member -MemberType NoteProperty -Name Files -Value $_j.files
-        }
-    }
-
-    # Allow casting or explicit import from the raw Content of an API call
-    PasswordPush([string]$JsonResponse) {
-        Write-Debug 'New PasswordPush object instantiated from JsonResponse string'
-        Initialize-PassPushPosh # Initialize the module if not yet done.
-
-        $_j = $JsonResponse | ConvertFrom-Json
         $this.Note = $_j.note
         $this.Payload = $_j.payload
         $this.IsExpired = $_j.expired
